f0c528abe55e4de63adaa9b30e80ca7fe0ca6f9b
PhamQuocTuan041203 <thotrangpro2@gmail.com>
2023-11-20 22:35:31 +0700

    update

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -5,9 +5,6 @@ namespace GUI
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
